225b29d670fc9621395d1945537551c3fdd73d1a
Prnchimanshu9113 <107669991+Prnchimanshu9113@users.noreply.github.com>
2024-10-07 16:06:33 +0530

    Create MacOS_Version.sh

--- MacOS_Version.sh
+++ MacOS_Version.sh
@@ -2,3 +2,6 @@
 
 OsVer="`system_profiler SPSoftwareDataType | grep "System Version: " | sed 's/System Version: //'`"
 echo "<result>$OsVer</result>"
+
+macos_version=$(sw_vers -productVersion)
+echo "<result>$macos_version</result>"
