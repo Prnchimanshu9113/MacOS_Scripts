400ed8d2e8718b7dfabd0c6fc95384d531056484
Prnchimanshu9113 <107669991+Prnchimanshu9113@users.noreply.github.com>
2024-10-07 16:31:33 +0530

    Create SIP_Status.sh

--- SIP_Status.sh
+++ SIP_Status.sh
@@ -7,3 +7,14 @@ if [ "$sip_status" != "" ]; then
 else
     echo "<result>Disabled</result>"
 fi
+
+<<'EOF'
+SIP (System Integrity Protection) is a security technology that restricts the root (superuser) account's access to certain system files and directories, even if you have administrator privileges. 
+It prevents the root user (or any other user) from modifying protected areas of the macOS operating system, including:
+
+/System
+/usr (except /usr/local)
+/bin
+/sbin
+Apps pre-installed by Apple (like Safari) are also protected by SIP.
+EOF
